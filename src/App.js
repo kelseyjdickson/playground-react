5c089d48cc30f5aaa568ebf1e56f1d739702ac0a
kelseyjdickson <kelseymedved@gmail.com>
2020-11-19 15:41:11 -0500

    added about page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 // import LandingPage from './LandingPage'
 import NavBar from './components/NavBar/NavBar'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <About />
       {/* <LandingPage /> */}
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+let About = () => {
+
+    return(
+        <h1>Hi, I am Kelsey! </h1>
+    )
+}
+
+export default About
